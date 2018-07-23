966410d70d037c0591586eefd746f1a033b7b902
prakhar dubey <prakhardb10@gmail.com>
2018-07-23 22:46:00 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import SearchBar from './containers/Search_bar';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
